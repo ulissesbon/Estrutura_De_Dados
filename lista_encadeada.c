c0dafb4bf97f7abc54994f9c76e08cd691f1b266
ulissesbon <ulisses.bonfim06@aluno.ifce.edu.br>
2024-04-08 09:11:09 -0300

    feat: Função 'ordenar', função 'trocar' e implementar ordenação na função 'inserir_ordem'

--- lista_encadeada.c
+++ lista_encadeada.c
@@ -25,11 +25,41 @@ struct no *buscar(struct no *lista, int valor){
 
 void listar(struct no *lista){
     struct no *aux = lista;
+
+    printf("[ ");
+
     while(aux != NULL){
-        printf("%d\n", aux->valor);
+        printf("%d ", aux->valor);
         aux = aux->prox;
     }
+
+    printf("]\n");
 }
+
+void trocar(struct no *i, struct no *j){
+    int aux = i->valor;
+    i->valor = j->valor;
+    j->valor = aux;
+}
+
+void ordenar(struct no *lista){
+    
+    struct no *aux_i = lista;
+    
+    while(aux_i != NULL){
+        struct no *aux_j = aux_i->prox;
+        while(aux_j != NULL){
+            if(aux_i->valor > aux_j->valor){
+                trocar(aux_i, aux_j);
+            }
+        aux_j = aux_j->prox;
+        }
+        aux_i = aux_i->prox;
+    }
+    
+
+}
+
 
 void alterar(struct no *lista, int valor, int novo_valor){
     struct no *aux = buscar(lista, valor);
@@ -37,6 +67,39 @@ void alterar(struct no *lista, int valor, int novo_valor){
         aux->valor = novo_valor;
     }
     
+}
+
+struct no *pegar_anterior(struct no *lista, struct no *no){
+
+    struct no *aux = lista;
+    while(aux != NULL && aux->prox != no){
+        aux = aux->prox;
+    }
+    return aux;
+
+}
+
+int remover(struct no **lista, int valor){
+    struct no *aux = buscar(*lista, valor);
+    if(*lista == NULL || aux == NULL){
+        return -1;
+    }
+    if(aux == *lista){
+        *lista = (*lista)->prox;
+        free(aux);
+    }
+    else if(aux->prox == NULL){
+        struct no *ant = pegar_anterior(*lista, aux);
+        ant->prox = NULL;
+        free(aux);
+    }
+    else{
+        struct no *ant = pegar_anterior(*lista, aux);
+        ant->prox = aux->prox;
+        free(aux);
+    }
+
+    return 0;
 }
 
 
@@ -59,20 +122,19 @@ void inserir_fim(struct no **lista, int valor){
 
     struct no *aux = malloc(sizeof(struct no));
     aux->valor = valor;
+    aux->prox = NULL;
+
     if (*lista == NULL){
-        aux->prox = NULL;
         *lista = aux;
+        return;
     }
-    else{
-        struct no *aux2 = *lista;
-        while(aux2->prox != NULL){
-            aux2 = aux2->prox;
-        }
-        aux2->prox = aux;
-    }
+    struct no *ant = pegar_anterior(*lista, NULL);
+        ant->prox = aux;
 }
 
 void inserir_ordem(struct no **lista, int valor){
+
+    ordenar(*lista);
 
     struct no *aux = malloc(sizeof(struct no));
     aux->valor = valor;
@@ -83,41 +145,25 @@ void inserir_ordem(struct no **lista, int valor){
         return;
     }
 
-    if((*lista)->prox == NULL){
-        if((*lista)->valor <= valor){
-            (*lista)->prox = aux;
-            aux->prox = NULL;
-            return;
-        }
-        else{
-            aux->prox = *lista;
-            *lista = aux;
-            return;
-            
-        }
-    }
 
-    if((*lista)->valor > valor){
-        aux->prox = *lista;
-        *lista = aux;
+    struct no *anterior = *lista;
+    struct no *proximo = *lista;
+    
+    if(valor < (*lista)->valor){
+        inserir_comeco(&(*lista), valor);
         return;
     }
 
-    struct no *anterior = *lista;
-    struct no *atual = (*lista)->prox;
-    
-    while(atual->valor < valor && atual->prox !=NULL){
-        anterior = anterior->prox;
-        atual = atual->prox;
+    while(proximo->valor < valor && proximo->prox != NULL){
+        proximo = proximo->prox;
     }
-    if(atual->prox == NULL){
-        aux->prox = NULL;
-        atual->prox = aux;
+    if(proximo->prox == NULL){
+        inserir_fim(&(*lista), valor);
+        return;
     }
-    else{
-        aux->prox = atual;
-        anterior->prox = aux;
-    }
+    anterior = pegar_anterior(*lista, proximo);
+    anterior->prox = aux;
+    aux->prox = proximo;
 
 }
 
@@ -126,15 +172,16 @@ int main(){
 
     struct no *ls = NULL;
 
-    inserir_ordem(&ls, 15);
     inserir_ordem(&ls, 10);
-    inserir_ordem(&ls, 1226524);
-    inserir_ordem(&ls, 1565);
-    inserir_ordem(&ls, 140);
-    inserir_ordem(&ls, 7);
+    inserir_fim(&ls, 77);
+    inserir_ordem(&ls, 51345);
+    inserir_comeco(&ls, 37);
+    inserir_ordem(&ls, 42);
+    inserir_comeco(&ls, 15);
+    inserir_fim(&ls, 20);
+    inserir_ordem(&ls, 30);
     
     listar(ls);
-
-
+    
 return 0;
 }
